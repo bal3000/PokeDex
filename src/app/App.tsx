946587d9d867190b26b7d2278e1d3c25962b5eb1
bal3000 <bal3000286@googlemail.com>
2020-06-04 08:16:37 +0100

    started building the structure/layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,50 +1,18 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Pokedex from './pokedex/Pokedex';
 
 import './App.scss';
-import { AppState } from '../store';
-import { loadPokemonList } from '../store/pokemon/actions';
-import { LinkingResource } from '../models/linking-resource.interface';
 
-interface AppProps {
-  pokemonList?: LinkingResource[];
-  loading: boolean;
-  loadPokemonList: any;
-}
-
-function App({ pokemonList, loading, loadPokemonList }: AppProps): JSX.Element {
-  useEffect(() => {
-    try {
-      loadPokemonList({ offset: 0, limit: 50 });
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
+function App(): JSX.Element {
   return (
     <div className="container-fluid">
-      <div className="row justify-content-md-center">
-        <div className="col-md-auto">
-          {loading ? (
-            <h1>LOADING...</h1>
-          ) : (
-            <ul>
-              {pokemonList?.map((pokemon) => (
-                <li key={pokemon.name}>{pokemon.name}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
+      <Switch>
+        <Route path="/pokedex" component={Pokedex} />
+      </Switch>
     </div>
   );
 }
 
-const mapStateToProps = (state: AppState) => {
-  return {
-    pokemonList: state.pokemon?.results,
-    loading: state.loading,
-  };
-};
-
-export default connect(mapStateToProps, { loadPokemonList })(App);
+export default App;
---
+++ src/app/pokedex/Pokedex.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import './Pokedex.scss';
+
+import { AppState } from '../../store';
+import { loadPokemonList } from '../../store/pokemon/actions';
+import { LinkingResource } from '../../models/linking-resource.interface';
+import Spinner from '../common/spinner/Spinner';
+
+interface PokedexProps {
+  pokemonList?: LinkingResource[];
+  loading: boolean;
+  loadPokemonList: any;
+}
+
+function Pokedex({
+  pokemonList,
+  loading,
+  loadPokemonList,
+}: PokedexProps): JSX.Element {
+  useEffect(() => {
+    try {
+      loadPokemonList({ offset: 0, limit: 50 });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const getNumber = (pokemon: LinkingResource): string => {
+    const split = pokemon.url.split('/');
+    return split[split.length - 2];
+  };
+
+  return (
+    <div className="row">
+      <div className="col-8">
+        {/* poke image here on select otherwise placeholder */}
+      </div>
+      <div className="col-4">
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className="list-group">
+            {pokemonList?.map((pokemon) => (
+              <a
+                href="#"
+                key={pokemon.name}
+                className="list-group-item list-group-item-action"
+              >
+                No. {getNumber(pokemon)} {pokemon.name}
+              </a>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state: AppState) => {
+  return {
+    pokemonList: state.pokemon?.results,
+    loading: state.loading,
+  };
+};
+
+export default connect(mapStateToProps, { loadPokemonList })(Pokedex);
